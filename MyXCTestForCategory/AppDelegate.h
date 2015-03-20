eb8d12af70e5c2f2ef897e25c105347fbbc2de5c
changyou <changyou0730@gmail.com>
2015-03-20 13:26:35 +0800

    Initial Commit

---
+++ MyXCTestForCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyXCTestForCategory
+//
+//  Created by chang on 15/3/20.
+//  Copyright (c) 2015年 chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyXCTestForCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyXCTestForCategory
+//
+//  Created by chang on 15/3/20.
+//  Copyright (c) 2015年 chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
